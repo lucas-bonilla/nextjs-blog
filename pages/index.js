c9be096eeb5612146d9e8a07930e0d7538086259
Lucas Bonilla Cabeza <lucas.bonilla@sectoralarm.eu>
2023-03-14 10:39:33 +0100

    testing

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,21 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
+//useSWR allows the use of SWR inside function components
+import useSWR from 'swr';
+
+//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+
 export default function Home({ allPostsData }) {
+  const { data, error } = useSWR('/api/staticdata', fetcher);
+    //Handle the error state
+    if (error) return <div>Failed to load</div>;
+    //Handle the loading state
+    if (!data) return <div>Loading...</div>;
+    //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+    const obj = JSON.parse(data);
   return (
     <Layout home>
       <Head>
@@ -32,7 +46,29 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+        <div>
+        <h1>My data from file</h1>
+        <ul>
+          <li>Name: {obj.record.name}</li>
+          <li>Language: {obj.record.id}</li>
+        </ul>
+      </div>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Daily TV</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
+    
   )
 }
 
@@ -44,3 +80,25 @@ export async function getStaticProps() {
     }
   }
 }
+
+
+// export default function Index() {
+//   //Set up SWR to run the fetcher function when calling "/api/staticdata"
+//   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+//   const { data, error } = useSWR('/api/staticdata', fetcher);
+
+//   //Handle the error state
+//   if (error) return <div>Failed to load</div>;
+//   //Handle the loading state
+//   if (!data) return <div>Loading...</div>;
+//   //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+//   return (
+//     <div>
+//       <h1>My Framework from file</h1>
+//       <ul>
+//         <li>Name: {data.record.name}</li>
+//         <li>Language: {data.record.language}</li>
+//       </ul>
+//     </div>
+//   );
+// }
